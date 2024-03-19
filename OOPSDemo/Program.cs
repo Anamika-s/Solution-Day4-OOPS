ee783db0cc55a7b000244b2a114579a812d04751
Anamika-s <anamika.sawhney22@gmail.com>
2024-03-19 09:35:27 +0530

    Add project files.

---
+++ OOPSDemo/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace OOPSDemo
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // //declaration
+           // Student student;
+           // // initialization
+           // student= new Student();
+           //// dec & init are done together
+           // Student student1 = new Student();
+           // // Here student a7 student are objects of class Student
+           // // int i;
+           // student.GetDetails();
+           // student.DisplayDetails();
+
+            //Student student1 = new Student();
+            //// new keyword will do two things
+            //// 1. It allocates memory for the object frm heap
+            //// 2. It initializes all the member variables to their def values
+            //student1.GetDetails();
+            //student1.DisplayDetails();
+
+            //Student student2 = new Student();
+            //student2.GetDetails();
+            //student2.DisplayDetails();
+
+            //Student student3 = new Student();
+            //student3.GetDetails();
+            //student3.DisplayDetails();
+
+            // Array of Objects
+
+            //Student[] students = new Student[10];
+            //for(int i=0;i<3;i++)
+            //{
+            //    students[i] = new Student();
+            //    students[i].GetDetails();
+               
+
+            //}
+
+            //foreach(Student student in students)
+            //{
+            //   student.DisplayDetails();
+            //}
+
+           //ArrayList list = new ArrayList(); 
+           //Student student1 = new Student();
+
+           // list.Add(student1);
+
+           // Student student2 = new Student();
+           // list.Add(student2);
+
+           // list.Add(1);
+           // list.Add("aaa");
+
+           // foreach(Student student in list)
+           // {
+           //     student.DisplayDetails();
+           // }
+
+            // use generic collection
+            List<Student> students = new List<Student>();
+            //Student student = new Student();
+            //student.GetDetails();
+            //students.Add(student);
+            string ch = "y";
+            int i = 0;
+            Student student;
+            while(ch=="y")
+            {    Console.WriteLine($"ENter Detaiks of STudent No {++i}");
+                student = new Student();
+                student.GetDetails();
+                students.Add(student);
+                Console.WriteLine("Do you want to add mire records");
+                ch = Console.ReadLine();
+            }
+
+
+
+           foreach (Student temp in students)
+            {
+                temp.DisplayDetails();
+            }
+
+
+
+
+        }
+    }
+}
---
+++ OOPSDemo/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    // A class shoud contain variables, properties, methods /...
+    // by def, all the members of a class are privte
+    // which means we cant acces them outside the class
+    // by def class is internal , that we can access the class anywhere in the same  project
+    // encapsulation > hiding the details frm the user
+    // abstarction > showing the needed details to the user
+    // we acheive both enc &* abst using access specifiers
+ // whatever we want to hide , make them private
+ // whatever we have to display, make them public
+
+    class Student
+    
+    {
+        int rn;
+        string name;
+        string batch;
+        int marks;
+        public void GetDetails()
+        {
+            Console.WriteLine("Enter RollNo");
+            rn = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Batch Name");
+            batch = Console.ReadLine();
+            Console.WriteLine("Enter Marks");
+            marks = Byte.Parse(Console.ReadLine());
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"RollNo is {rn}");
+            Console.WriteLine($"Name is {name}");
+            Console.WriteLine($"Batch is {batch}");
+            Console.WriteLine($"Marks are {marks}");
+        }
+    }
+}
